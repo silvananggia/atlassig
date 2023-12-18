06035c87a7a8d8a1b698d5b7bd4686586bd0db95
Silvan Anggia Bayu <silv007@brin.go.id>
2023-12-18 14:36:07 +0700

    Update

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: 'http://172.23.73.42/api',
+    baseURL: 'http://localhost:9000/api',
     headers: {'Content-Type': 'application/json'},
     
 });
